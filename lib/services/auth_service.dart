bac118ea7a2daec166e89bcade1ab51ee1211e71
emzaro731-byte <emzaro731@gmail.com>
2026-09-16 13:45:21 +0100

    Add Firebase Google authentication service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn.instance.authenticate();
+    final googleAuth = googleUser.authentication;
+
+    final idToken = googleAuth.idToken;
+    if (idToken == null) {
+      throw FirebaseAuthException(
+        code: 'missing-google-id-token',
+        message: 'Google did not return an ID token.',
+      );
+    }
+
+    final credential = GoogleAuthProvider.credential(idToken: idToken);
+    return _auth.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    await GoogleSignIn.instance.signOut();
+    await _auth.signOut();
+  }
+}
